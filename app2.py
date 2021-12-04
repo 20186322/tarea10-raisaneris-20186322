0b581bb5cbf901ffe77cb7d611c25651489df582
20186322 <67034547+20186322@users.noreply.github.com>
2021-12-03 20:22:38 -0400

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional, Text
+from datetime import datetime
+from uuid import uuid4 as uuid
+import uvicorn
+
+app = FastAPI()
+
+posts = []
+
+class Post(BaseModel):
+    Correo: str
+    Nombre: str
+    Apellido: str
+    RNC: str
+    Telefono: str
+
+@app.get('/')
+def read_root():
+    return {"Bienvenidos": "Bienvenidos a mi API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts')
+def save_post(post: Post):
+    post.id = str(uuid())
+    posts.append(post.dict())
+    return posts[-1]
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: str):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Item not found")
+
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"message": "Factura ha sido eliminada exitosamente"}
+    raise HTTPException(status_code=404, detail="Item not found")
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: str, updatedPost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["Correo"]= updatedPost.dict()["Correo"]
+            posts[index]["Nombre"]= updatedPost.dict()["Nombre"]
+            posts[index]["Apellido"]= updatedPost.dict()["Apellido"]
+            posts[index]["RNC"]= updatedPost.dict()["RNC"]
+            posts[index]["Telefono"]= updatedPost.dict()["Telefono"]
+            return {"message": "La factura se ha actualizado exitosamente"}
+    raise HTTPException(status_code=404, detail="Item not found")
